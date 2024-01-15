f2ea0539c1d36f868aa1272a96cb2df79d573601
khangluong2004 <khangluong2004@gmail.com>
2024-01-16 00:00:57 +1100

    Fix Cognito Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,23 @@ import awsExport from './aws-export.js'
 
 Amplify.configure({
   Auth: {
-    region: awsExport.REGION,
-    userPoolId: awsExport.USER_POOL_ID,
-    userPoolWebClientId: awsExport.USER_POOL_APP_CLIENT_ID
+      Cognito: {
+        region: awsExport.REGION,
+        userPoolId: awsExport.USER_POOL_ID,
+        userPoolClientId: awsExport.USER_POOL_APP_CLIENT_ID,
+        loginWith: { 
+          oauth: {
+            domain: 'https://tictactoe.auth.ap-southeast-2.amazoncognito.com',
+            scopes: ['openid email phone'],
+            redirectSignIn: ['http://localhost:5173/'],
+            redirectSignOut: ['https://www.google.com/'],
+            responseType: 'code',
+          },
+          username: 'false',
+          email: 'true', 
+          phone: 'true', 
+        }
+      }
   }
 })
 
@@ -22,12 +36,16 @@ function App() {
       {({signOut, user}) => {
           console.log(user);
 
-          return( 
-            <div className="flex items-center justify-center py-10 h-dvh">
-              <h2> Welcome, {user.email} </h2>
-              <button onClick={signOut}> Sign out </button>
-              <Board />
-            </div>
+          return(
+            <div>
+              <div className="flex justify-center items-center pt-10">
+                <h2 className="font-bold p-2"> Welcome </h2>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4" onClick={signOut}> Sign out </button>
+              </div>
+              <div className="flex items-center justify-center pb-10 h-dvh">
+                <Board />
+              </div>
+            </div> 
           )
         }
       }
