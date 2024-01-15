e287c1ce13be396e0c73600fd750882202a6ead8
khangluong2004 <khangluong2004@gmail.com>
2024-01-15 23:13:42 +1100

    Add AWS authenticator setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import Board from "./Board.jsx"
 
+// AWS Cognito authenticator setup
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+// Configure AWS Amplify to connect to AWS Cognito
+import {Amplify} from 'aws-amplify'
+import awsExport from './aws-export.js'
+
+Amplify.configure({
+  Auth: {
+    region: awsExport.REGION,
+    userPoolId: awsExport.USER_POOL_ID,
+    userPoolWebClientId: awsExport.USER_POOL_APP_CLIENT_ID
+  }
+})
+
 function App() {
   return (
-    <div className="flex items-center justify-center py-10 h-dvh">
-      <Board />
-    </div>
+    <Authenticator>
+      {({signOut, user}) => {
+          console.log(user);
+
+          return( 
+            <div className="flex items-center justify-center py-10 h-dvh">
+              <h2> Welcome, {user.email} </h2>
+              <button onClick={signOut}> Sign out </button>
+              <Board />
+            </div>
+          )
+        }
+      }
+    </Authenticator>
   )
 }
 
---
+++ src/aws-export.js
@@ -0,0 +1,5 @@
+export default {
+    "REGION": "ap-southeast-2",
+    "USER_POOL_ID": "ap-southeast-2_HsDQhTBjL",
+    "USER_POOL_APP_CLIENT_ID": "5bngn2bddl0j52gtf9nt34g680"
+}
